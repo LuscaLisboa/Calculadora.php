e9ee12728126369208de29d606adf38d2ffdd1c5
MarlonVolff <marlonvolffsilva@gmail.com>
2024-05-06 20:04:08 -0300

    ATT CALC

---
+++ Calculadora.php-main/Calculadora/Calculadora.php
@@ -0,0 +1,117 @@
+<?php
+session_start();
+
+include 'Funcoes.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+    <link rel="stylesheet" href="Style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="calculadora">
+        <h1 class="txtcal">Calculadora</h1>
+
+        <form method="get" action="">
+            <!-- 1° Número -->
+            <input type="text" name="num1" placeholder="Primeiro numero">
+            <!-- Operador -->
+            <select name="oper" required>
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+                <option value="^">^</option>
+                <option value="!">!</option>
+            </select>
+            <!-- 2° Número -->
+            <input type="text" name="num2" placeholder="Segundo numero">
+            <br>
+            <input class="calcula" type="submit" value="Calcular">
+            <div class="container">
+                <button name="salvar" class="btn-amarelo">Salvar</button>
+                <button name="pegar" class="btn-cinza">Pegar Valores</button>
+                <button name="m" value="memoria" class="btn-azul">M</button>
+                <button name="limpar" value="apagar" class="btn-azul">Apagar Historico</button>
+            </div>
+        </form>
+
+
+        <?php
+
+    $num1 = intval($_GET['num1']);
+    $oper = $_GET['oper'];
+    $num2 = intval($_GET['num2']);
+    
+    if (isset($_GET['num1']) && isset($_GET['oper'])) {
+
+
+        $res = Calcular($num1, $oper, $num2); 
+
+        $texto = "";
+
+        if($num2 != null){
+            $texto = "$num1 $oper $num2 = $res";
+            echo "<div class=\"resposta\">
+                    $texto
+                </div>";
+        }
+        elseif($num2 == null && $num1 != null){
+            $texto = "$num1 $oper = $res";
+            echo "<div class=\"resposta\">
+                    $texto
+                </div>";
+        }
+        
+        if($texto != '') SalvarHistorico($texto);
+        
+    }
+
+    if(isset($_GET['salvar'])) Salvar($num1, $oper, $num2);
+    if (isset($_GET['pegar'])) Pegar();
+
+    if (isset($_GET['m'])) {
+        if (!isset($_SESSION['toggle'])) {
+            $_SESSION['toggle'] = false;
+        }
+    
+        if ($_SESSION['toggle']) Salvar($num1, $oper, $num2);
+        else {
+            Pegar();
+        }
+    
+        $_SESSION['toggle'] = !$_SESSION['toggle'];
+    }
+
+    if(isset($_GET['limpar']) && $_GET['limpar'] == "apagar") LimparSessoes();
+
+    ?>
+
+        <p class="Historico">⬐ Histórico ⬎</p>
+
+        <?php
+
+    if(isset($_SESSION['historico'])){
+        foreach ($_SESSION['historico'] as $operacao){
+            echo "<div class=\"historico\">
+                    $operacao
+                  </div>";
+        }
+    }
+
+    echo "</div>";
+
+    ?>
+
+</body>
+
+</html>
